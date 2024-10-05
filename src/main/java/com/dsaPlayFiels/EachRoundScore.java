6f6fac5367c4726e849d55ef70693a42c4a9b8bd
Sai Nishanth Vaka <sainishanthvaka76@gmail.com>
2024-10-05 12:00:08 -0500

    feat: each round score

--- src/main/java/com/dsaPlayFiels/EachRoundScore.java
+++ src/main/java/com/dsaPlayFiels/EachRoundScore.java
@@ -1,2 +1,48 @@
-package com.dsaPlayFiels;public class PlayerGameScore {
+package com.dsaPlayFiels;
+
+public class EachRoundScore {
+    private int score = 0;
+    private boolean coOperate = false;
+    private int round = 0;
+    private void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    private void setCoOperate(boolean coOperate) {
+        this.coOperate = coOperate;
+    }
+
+    public boolean getCoOperate() {
+        return coOperate;
+    }
+
+    private void setRound(int round) {
+        this.round = round;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public void updateMove(int score, boolean coOperate) {
+        setScore(score);
+        setCoOperate(coOperate);
+    }
+
+    public EachRoundScore() {
+    }
+
+    public EachRoundScore(int round) {
+        setRound(round);
+    }
+
+    public EachRoundScore(int score, boolean coOperate, int round) {
+        setScore(score);
+        setCoOperate(coOperate);
+        setRound(round);
+    }
 }
