e063610b10f167ba247d410c8c3d1286109f0fd5
Sai Nishanth Vaka <sainishanthvaka76@gmail.com>
2024-10-06 23:53:47 -0500

    feat: series and game

--- src/main/java/com/dsaPlayFiels/Game.java
+++ src/main/java/com/dsaPlayFiels/Game.java
@@ -1,2 +1,70 @@
-package com.dsaPlayFiels;public class game {
+package com.dsaPlayFiels;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Game {
+    private int noOfPlayers = 2;
+    private int coOperatePoints = 5;
+    private int minNoCoOperatePoints = 0;
+    private int maxNoOperatePoints = 10;
+    private String player1, player2;
+    private boolean player1CoOperate = false, player2CoOperate = false;
+    private int round;
+
+    private void setNoOfPlayers(int noOfPlayers) {
+        this.noOfPlayers = noOfPlayers;
+    }
+
+    private void setCoOperatePoints(int coOperatePoints) {
+        this.coOperatePoints = coOperatePoints;
+    }
+
+    private void setMinNoCoOperatePoints(int minNoCoOperatePoints) {
+        this.minNoCoOperatePoints = minNoCoOperatePoints;
+    }
+
+    private void setMaxNoOperatePoints(int maxNoOperatePoints) {
+        this.maxNoOperatePoints = maxNoOperatePoints;
+    }
+
+    private Map<String, EachRoundScore> evaluatePoints() {
+        Map<String, EachRoundScore> map = new HashMap<>();
+        if (player1CoOperate && player2CoOperate) {
+            map.put(player1, new EachRoundScore(minNoCoOperatePoints, player1CoOperate, round));
+            map.put(player2, new EachRoundScore(minNoCoOperatePoints, player2CoOperate, round));
+        }
+        else if (!player1CoOperate && !player2CoOperate) {
+            map.put(player1, new EachRoundScore(minNoCoOperatePoints, player1CoOperate, round));
+            map.put(player2, new EachRoundScore(minNoCoOperatePoints, player2CoOperate, round));
+        }
+        else if (player1CoOperate && !player2CoOperate) {
+            map.put(player1, new EachRoundScore(maxNoOperatePoints, player1CoOperate, round));
+            map.put(player2, new EachRoundScore(minNoCoOperatePoints, player2CoOperate, round));
+        }
+        else if (player2CoOperate && !player1CoOperate) {
+            map.put(player1, new EachRoundScore(minNoCoOperatePoints, player1CoOperate, round));
+            map.put(player2, new EachRoundScore(maxNoOperatePoints, player2CoOperate, round));
+        }
+        return map;
+    }
+
+
+    public Map<String, EachRoundScore> runGame(String player1, String player2, int round) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.round = round;
+        System.out.print(player1 + " please choose 1 to Co-operate and 2 to not: ");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.nextInt() == 1) this.player1CoOperate = true;
+        System.out.print(player2 + " please choose 1 to Co-operate and 2 to not: ");
+        scanner = new Scanner(System.in);
+        if (scanner.nextInt() == 1) this.player2CoOperate = true;
+        return evaluatePoints();
+    }
+
+
+    public Game() {
+    }
 }
--- src/main/java/com/dsaPlayFiels/PrisonersDilemma.java
+++ src/main/java/com/dsaPlayFiels/PrisonersDilemma.java
@@ -1,4 +1,52 @@
 package com.dsaPlayFiels;
 
+import java.util.Map;
+
 public class PrisonersDilemma {
+    private Game game;
+    private int seriesLength = 3;
+    private int goal = 15;
+    private Player player1;
+    private Player player2;
+
+
+    public void setSeries() {
+        this.game = new Game();
+    }
+
+    public PrisonersDilemma() {
+        player1 = new Player();
+        player2 = new Player();
+    }
+
+    public void playSeries() {
+        for (int i = 0; i < seriesLength; i++) {
+            game = new Game();
+            Map<String, EachRoundScore> result =
+                    game.runGame(player1.getPlayerId(), player2.getPlayerId(), i + 1);
+            player1.updateCurrentMove(result.get(player1.getPlayerId()));
+            player2.updateCurrentMove(result.get(player2.getPlayerId()));
+            if (player1.getNetScore() >= goal) {
+                System.out.println(player1.getPlayerId() + " wins!");
+                return;
+            }
+            if (player2.getNetScore() >= goal) {
+                System.out.println(player2.getPlayerId() + " wins!");
+                return;
+            }
+        }
+        results();
+    }
+
+    private void results() {
+        if (player1.getNetScore() > player2.getNetScore()) {
+            System.out.println(player1.getPlayerId() + " wins!");
+            return;
+        }
+        if (player2.getNetScore() > player1.getNetScore()) {
+            System.out.println(player2.getPlayerId() + " wins!");
+            return;
+        }
+        System.out.println("It's a draw!");
+    }
 }
